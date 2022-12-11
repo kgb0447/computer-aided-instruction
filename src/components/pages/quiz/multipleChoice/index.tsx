c37f2d27efbbe17de8765535bbf7c9d929de3567
Bennick Garcia <kaelgarciabennick1997@gmail.com>
2022-12-11 11:52:35 +0800

    featL (SKGCRUM-r1218)added radio form for quizPage

--- src/components/pages/quiz/multipleChoice/index.tsx
+++ src/components/pages/quiz/multipleChoice/index.tsx
@@ -1,34 +1,46 @@
-import React, { useEffect } from 'react'
+import React, { useRef, useState } from 'react'
 import { multipleChoiceQuestions } from '../../../../data/multipleChoiceQuestions'
 import styles from './style.module.scss'
 import useArrayMapper from '../../../../hooks/useArrayMapper'
 import { Header } from '../../main/componets/header';
+import { type } from '@testing-library/user-event/dist/type';
+
 export default function MultipleChoice() {
   
   const quizQuestion = useArrayMapper(multipleChoiceQuestions, "Question");
   const quizChoices = useArrayMapper(multipleChoiceQuestions, "Choices");
-  const handleSubmit = (item : string) =>{
-    const answer = multipleChoiceQuestions[0].Items[0].Choices.filter(data => data === item)
-    console.log(answer,"Test answer");
-    if(answer[0] === multipleChoiceQuestions[0].Items[0].Answer ){
-      console.log("correct")
-    } else{
-      console.log("wrong");
+
+  const myData = ['a','b','c','d'];
+
+const radioRef = useRef(null)
+const handleSubmit = (e:any)=>{
+  e.preventDefault();
+ 
+  for(let value of e.target.children){
+    if(value.checked){
+      console.log(value.value,"test")
     }
   }
-
- 
+  
+  
+}
   return (
     <div className={styles.multipleChoice_container}>
       <Header/>
       <div className={styles.quiz_container}>
         <div className={styles.question}>{quizQuestion}</div>
         <div className={styles.choices_wrapper} >
+      <form action="submit" onSubmit={handleSubmit} ref={radioRef}>
       {
-        quizChoices.map((item,index)=>(
-            <button key={index}>{item}</button>
+        myData.map((item,index)=>(
+          <React.Fragment key={index}>
+            <input type="radio" name='choices' value={item} />{item}
+            </React.Fragment>
         ))
+        
       }
+      <input type="submit" value="Submit" />
+      </form>
        </div>
       </div>
     </div>

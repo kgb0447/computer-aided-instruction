37aab7e33b9cfcea9327ccb2432762ce84228232
Bennick Garcia <kaelgarciabennick1997@gmail.com>
2022-12-13 22:23:22 +0800

    fix: (SKGCRUM-1218)fixed quiz radio rendering and added styles

--- src/components/pages/quiz/multipleChoice/index.tsx
+++ src/components/pages/quiz/multipleChoice/index.tsx
@@ -10,28 +10,37 @@ export default function MultipleChoice() {
   // const quizQuestion = useArrayMapper(multipleChoiceQuestions, "Question");
   // const quizChoices = useArrayMapper(multipleChoiceQuestions, "Choices");
   const [selectedQuizCat,setSelectedQuizCat] = useState<any>(null)
+  const [randomNumber,setRandomNumber] = useState<number|null>(null)
  
 
   const handleSelectTopic = (value:any) =>{
     const getIndexOfSelectedCategory =  multipleChoiceQuestions.filter((item:any)=> item === value)[0].Category;
     const getMultipleChoiceQuestionsCategories = multipleChoiceQuestions.map((item:any) => item.Category)
     const selectedIndex = getMultipleChoiceQuestionsCategories.indexOf(getIndexOfSelectedCategory);
-    setSelectedQuizCat(multipleChoiceQuestions[selectedIndex].Items)
+    setSelectedQuizCat(multipleChoiceQuestions[selectedIndex].Items);
+    if(selectedQuizCat !== null) {
+      const randomNum = Math.floor(Math.random() * ( selectedQuizCat.length - 0) + 0);
+      setRandomNumber(randomNum)
+    }
     
   }
-
 
 const handleSubmit = (e:any)=>{
   e.preventDefault();
   for(let value of e.target.children){
     if(value.checked){
-      console.log(value,"test")
+      console.log(value.value,"test")
     }
   }
-  console.log(e.target.children)
+  // console.log(e.target.children)
 }
-console.log(selectedQuizCat);
-
+console.log(selectedQuizCat,"Test");
+const handleNext = () =>{
+  if(selectedQuizCat !== null) {
+    const randomNum = Math.floor(Math.random() * ( selectedQuizCat.length - 0) + 0);
+    setRandomNumber(randomNum)
+  }
+}
   return (
     <div className={styles.multipleChoice_container}>
       <Header/>
@@ -44,9 +53,9 @@ console.log(selectedQuizCat);
           </React.Fragment>
         ))
       },
-      <form action="submit" onSubmit={handleSubmit}>
-        {selectedQuizCat[0].Question}
-      {
+      {/* <form action="submit" onSubmit={handleSubmit}> */}
+        {/* {selectedQuizCat[0].Question} */}
+      {/* {
          selectedQuizCat !== null ? (
           
           selectedQuizCat.map((item:any,index:number)=>(
@@ -58,14 +67,34 @@ console.log(selectedQuizCat);
                     <input type="radio" name="choices" value={val}/><span> {val}</span>
                   </React.Fragment>
                 ))
+
               }
             </React.Fragment>
           ))
          ) : null
+      } */}
+      <form action="submit" onSubmit={handleSubmit}>
+      {
+        selectedQuizCat && randomNumber !== null ? (
+          <div className={styles.question_container}>
+          {selectedQuizCat[randomNumber].Question}
+          {
+            selectedQuizCat[randomNumber].Choices.map((item:any,index:number)=>(
+              <div className={styles.choices_wrapper} key={index}>
+                <input type="radio" name="choices" value={item}/><label>{item}</label>
+              </div>
+            ))
+          }
+
+          </div>
+        ) : null
       }
-        <input type="submit" value={"Test"}/>
+      
+        <input type="submit" value={"Submit"}/>
       </form>
+      <button onClick={handleNext}>Next</button>
         </div>
+        
     </div>
   )
 }
